53e4800499c923ecad64522734b585050661f924
TELUS Emerging IoT <emergingiot@telus.com>
2020-02-29 18:26:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yasinkaya.121@gmail.com',
     description='sixfab linux libraries',
     license='MIT',
-    url='https://github.com/sixfab/Sixfab_RPi_CellularIoT_App_Shield',
+    url='https://github.com/TELUS-Emerging-IoT/TELUS_Sixfab_RPi_CellularIoT_App_Shield',
     dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.9.3'],
 	install_requires  = ['Adafruit-GPIO>=0.9.3', 'pyserial', 'adafruit-ads1x15'],
     packages=find_packages()
