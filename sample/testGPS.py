9ed1175d4bd88a8190e5c24e04a97ce11a2a2348
TELUS Emerging IoT <emergingiot@telus.com>
2020-02-29 20:58:44 -0500

    Update testGPS.py

--- sample/testGPS.py
+++ sample/testGPS.py
@@ -10,7 +10,6 @@ node.disable()          # clean up before anything else
 time.sleep(0.5)
 node.enable()
 time.sleep(0.5)
-node.powerUp()
  
 node.sendATComm("ATE1","OK\r\n")        # set echo ON
  
@@ -21,18 +20,18 @@ node.getHardwareInfo()          # get the module model e.g. BG96
 node.sendATComm("AT+CIMI","OK\r\n")     # get IMSI
  
 print("turning GPS on...\r\n")
-#node.turnOffGNSS()
+node.turnOffGNSS()
 time.sleep(1)
 node.turnOnGNSS()
 print("success...\r\n")
- 
-# get Latitude and Longitude 
+
+# get Latitude and Longitude
 try:
-   while 1:
-      node.sendATComm("AT+QGPSLOC=2","OK\r\n")  # get Long / Lat
-      time.sleep(15)                            # every 60 seconds
+   while 1:
+      node.sendATComm("AT+QGPSLOC=2","OK\r\n")  # get Long / Lat
+      time.sleep(15)                            # every 60 seconds
 except KeyboardInterrupt:
-   pass
+   pass
 
 time.sleep(1)
 node.turnOffGNSS()
