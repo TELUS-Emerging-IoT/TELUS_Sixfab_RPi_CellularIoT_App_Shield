d695998d49acc4166fcd79d45dff0c3b61e3a385
TELUS Emerging IoT <emergingiot@telus.com>
2020-02-29 21:25:45 -0500

    Update and rename buton_led_test.py to button_led_test.py

--- sample/button_led_test.py
+++ sample/button_led_test.py
@@ -1,9 +1,6 @@
 from cellulariot import cellulariot
 import time
 
-your_ip = "xx.xx.xx.xx" # change with your ip
-your_port = "xxxx" # change with your port
-
 #node = cellulariot.CellularIoT() # for Sixfab CellularIoT HAT
 node = cellulariot.CellularIoTApp() # for Sixfab CellularIoT App. Shield
 node.setupGPIO()
@@ -14,8 +11,16 @@ node.enable()
 time.sleep(1)
 node.powerUp()
 
-while True:
-	if (node.readUserButton() == 1):
-		node.turnOffUserLED()
-	else:
-		node.turnOnUserLED()
+print("\nPress USER button to turn on LED\r")
+print("Ctrl-C to cancel...\r\n")
+
+try:
+   while 1:
+      if (node.readUserButton() == 1):
+         node.turnOffUserLED()
+      else:
+         node.turnOnUserLED()
+except KeyboardInterrupt:
+   pass
+
+node.disable()
